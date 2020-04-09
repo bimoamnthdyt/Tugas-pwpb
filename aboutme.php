298cdb89e2986a57f348d5be938dc6f4913db702
Bimo Amanta Hidayat <55226891+bimoamnthdyt@users.noreply.github.com>
2020-04-09 19:25:02 +0700

    menambahkan nomor hp ,email dan mehapus alamat

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,8 @@
   echo "Nama : Bimo Amanta Hidayat";
   echo "Kelas : : XI RPL 1";
   echo "Asal Sekolah : SMK Taruna Bhakti Depok";
-  echo "Alamat Rumah : JL JASA WARGA NO.39 RT 005 RW 026 KEL BAKTIJAYA KOTA DEPOK";
+
+  echo "Nomor HP : 083898331298";
+  echo "Email : bimoamanta9@gmail.com";
 ?>
+ 
