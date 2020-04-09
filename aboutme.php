417a969dd1c38ddfbd426f04e7bc168d40fbbf03
Bimo Amanta Hidayat <55226891+bimoamnthdyt@users.noreply.github.com>
2020-04-09 19:18:10 +0700

    menambahkan nama,kelas,sekolah,alamat

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+  echo "Nama : Bimo Amanta Hidayat";
+  echo "Kelas : : XI RPL 1";
+  echo "Asal Sekolah : SMK Taruna Bhakti Depok";
+  echo "Alamat Rumah : JL JASA WARGA NO.39 RT 005 RW 026 KEL BAKTIJAYA KOTA DEPOK";
+?>
